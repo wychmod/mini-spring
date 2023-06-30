fb9565aa263a78d40d6f8a30d0bf1194b528ce2e
wychmod <545480453@qq.com>
2023-06-30 15:03:41 +0800

    完善Aop功能

--- src/main/java/com/wychmod/demo/aspect/LogAspect.java
+++ src/main/java/com/wychmod/demo/aspect/LogAspect.java
@@ -1,5 +1,6 @@
 package com.wychmod.demo.aspect;
 
+import com.wychmod.spring.framework.aop.aspect.JoinPoint;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -13,7 +14,8 @@ public class LogAspect {
     /**
      * 在调用一个方法之前，执行before方法
      */
-    public void before() {
+    public void before(JoinPoint joinPoint) {
+        joinPoint.setUserAttribute("startTime_" + joinPoint.getMethod().getName(), System.currentTimeMillis());
         //这个方法中的逻辑，是由我们自己写的
         log.info("Invoker Before Method!!!");
     }
@@ -21,15 +23,18 @@ public class LogAspect {
     /**
      * 在调用一个方法之后，执行after方法
      */
-    public void after(){
-        log.info("Invoker After Method!!!");
+    public void after(JoinPoint joinPoint){
+        long startTime = (Long)joinPoint.getUserAttribute("startTime_" + joinPoint.getMethod().getName());
+        long endTime = System.currentTimeMillis();
+
+        log.info("Invoker After Method!!! use time " + (endTime - startTime));
     }
 
     /**
      * 抛出异常执行
      */
-    public void afterThrowing(){
+    public void afterThrowing(JoinPoint joinPoint,Throwable ex){
 
-        log.info("出现异常");
+        log.info("出现异常" + ex.getMessage());
     }
 }
---
+++ src/main/java/com/wychmod/spring/framework/aop/aspect/JoinPoint.java
@@ -0,0 +1,20 @@
+package com.wychmod.spring.framework.aop.aspect;
+
+import java.lang.reflect.Method;
+
+/**
+ * @description: 切点接口
+ * @author: wychmod
+ * @date: 2023/6/30
+ */
+public interface JoinPoint {
+    Method getMethod();
+
+    Object[] getArguments();
+
+    Object getThis();
+
+    void setUserAttribute(String key,Object value);
+
+    Object getUserAttribute(String key);
+}
