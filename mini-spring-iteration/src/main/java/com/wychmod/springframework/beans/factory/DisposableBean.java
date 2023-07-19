4db56fa84e4015846b9016bd46139cf0a60be0a0
wychmod <545480453@qq.com>
2023-07-19 22:06:47 +0800

    Bean的注册表

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/DisposableBean.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/DisposableBean.java
@@ -1,7 +1,7 @@
 package com.wychmod.springframework.beans.factory;
 
 /**
- * @description:
+ * @description: 销毁时释放资源的 bean 实现的接口， 在释放时调用destroy()
  * @author: wychmod
  * @date: 2023/7/17
  */
--- mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -1,27 +1,61 @@
 package com.wychmod.springframework.beans.factory.support;
 
+import com.wychmod.springframework.beans.BeansException;
+import com.wychmod.springframework.beans.factory.DisposableBean;
 import com.wychmod.springframework.beans.factory.config.SingletonBeanRegistry;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * @description: 实现 getSingleton 方法，同时实现了一个受保护的 addSingleton 方法
- * 这个方法可以被继承此类的其他类调用。包括：AbstractBeanFactory 以及继承的 DefaultListableBeanFactory 调用。
+ * @description: 提供了对单例Bean的注册、获取和管理的功能，是Spring容器中单例Bean的注册表。
  * @author: wychmod
  * @date: 2023/7/14
  */
 public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 
-    private Map<String, Object> singletonObjects = new HashMap<>();
+    /**
+     * 空单例对象的内部标记：用作并发映射（不支持空值）的标记值。
+     */
+    protected static final Object NULL_OBJECT = new Object();
+
+    private Map<String, Object> singletonObjects = new ConcurrentHashMap<>();
+
+    private final Map<String, DisposableBean> disposableBeans = new LinkedHashMap<>();
 
     @Override
     public Object getSingleton(String beanName) {
         return singletonObjects.get(beanName);
     }
 
-    protected void addSingleton(String beanName, Object singletonObject) {
+    @Override
+    public void registerSingleton(String beanName, Object singletonObject) {
         singletonObjects.put(beanName, singletonObject);
+    }
+
+    public void registerDisposableBean(String beanName, DisposableBean bean) {
+        disposableBeans.put(beanName, bean);
+    }
+
+    /**
+     * 销毁单例时调用destroy()
+     */
+    public void destroySingletons() {
+        Set<String> keySet = this.disposableBeans.keySet();
+        Object[] disposableBeanNames = keySet.toArray();
+
+        for (int i = disposableBeanNames.length - 1; i >= 0; i--) {
+            Object beanName = disposableBeanNames[i];
+            DisposableBean disposableBean = disposableBeans.remove(beanName);
+            try {
+                disposableBean.destroy();
+            } catch (Exception e) {
+                throw new BeansException("Destroy method on bean with name '" + beanName + "' threw an exception", e);
+            }
+        }
     }
 
 }
