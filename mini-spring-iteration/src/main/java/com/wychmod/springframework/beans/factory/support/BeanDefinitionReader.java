afddcb61913cc130f8717fee75378f6e57311f95
wychmod <545480453@qq.com>
2023-07-21 15:39:32 +0800

    BeanDefinition的读取接口

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/BeanDefinitionReader.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/BeanDefinitionReader.java
@@ -5,20 +5,25 @@ import com.wychmod.springframework.core.io.Resource;
 import com.wychmod.springframework.core.io.ResourceLoader;
 
 /**
- * @description: Simple interface for bean definition readers.
+ * @description: BeanDefinition的读取接口.
  * getRegistry()、getResourceLoader()，都是用于提供给后面三个方法的工具，加载和注册，这两个方法的实现会包装到抽象类中，以免污染具体的接口实现方法。
  * @author: wychmod
  * @date: 2023/7/14
  */
 public interface BeanDefinitionReader {
 
+    /** 获取bean配置注册接口 */
     BeanDefinitionRegistry getRegistry();
 
+    /** 获取资源加载器 */
     ResourceLoader getResourceLoader();
 
+    /** 加载单个Bean定义 */
     void loadBeanDefinitions(Resource resource) throws BeansException;
 
     void loadBeanDefinitions(Resource... resources) throws BeansException;
 
     void loadBeanDefinitions(String location) throws BeansException;
+
+    void loadBeanDefinitions(String... locations) throws BeansException;
 }
