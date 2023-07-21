2248e5302bb289aff8d25bd91b0385097d7a8cb9
wychmod <545480453@qq.com>
2023-07-21 20:31:51 +0800

    在Spring容器创建和初始化Bean的过程中，自动将ApplicationContext实例注入到实现了ApplicationContextAware接口的Bean中。

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/context/support/ApplicationContextAwareProcessor.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/context/support/ApplicationContextAwareProcessor.java
@@ -1,12 +1,13 @@
 package com.wychmod.springframework.context.support;
 
 import com.wychmod.springframework.beans.BeansException;
-import com.wychmod.springframework.beans.factory.ApplicationContextAware;
 import com.wychmod.springframework.beans.factory.config.BeanPostProcessor;
 import com.wychmod.springframework.context.ApplicationContext;
+import com.wychmod.springframework.context.ApplicationContextAware;
 
 /**
  * @description: 通过 BeanPostProcessor 实现类感知应用上下文对象
+ * 在Spring容器创建和初始化Bean的过程中，自动将ApplicationContext实例注入到实现了ApplicationContextAware接口的Bean中。
  * @author: wychmod
  * @date: 2023/7/17
  */
