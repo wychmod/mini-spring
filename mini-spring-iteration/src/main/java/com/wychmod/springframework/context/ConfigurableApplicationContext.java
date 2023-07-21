db876d50c744ba29b3524e579b1eb29e453d94e2
wychmod <545480453@qq.com>
2023-07-21 16:37:02 +0800

    提供了对应用程序上下文的配置和管理的能力，使得应用程序可以更加灵活地进行配置和定制。

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/context/ConfigurableApplicationContext.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/context/ConfigurableApplicationContext.java
@@ -3,17 +3,26 @@ package com.wychmod.springframework.context;
 import com.wychmod.springframework.beans.BeansException;
 
 /**
- * @description: 接口配置应用上下文
+ * @description: 提供了对应用程序上下文的配置和管理的能力，使得应用程序可以更加灵活地进行配置和定制。
  * @author: wychmod
  * @date: 2023/7/17
  */
-public interface ConfigurableApplicationContext extends ApplicationContext{
+public interface ConfigurableApplicationContext extends ApplicationContext {
 
     /**
      * 刷新容器
-     *
-     * @throws BeansException
      */
     void refresh() throws BeansException;
 
+    void publishEvent(ApplicationEvent event);
+
+    /**
+     * 注册一个虚拟机钩子，关闭容器
+     */
+    void registerShutdownHook();
+
+    /**
+     * 关闭容器
+     */
+    void close();
 }
