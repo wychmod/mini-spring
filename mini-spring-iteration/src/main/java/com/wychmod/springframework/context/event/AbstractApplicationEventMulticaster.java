e49b645b727cdc4d4660b95714951c14c821eb03
wychmod <545480453@qq.com>
2023-07-21 22:22:18 +0800

    提供了将事件广播给所有注册的监听器的能力。它维护了一个监听器列表，并在事件发生时，遍历列表将事件发送给所有注册的监听器。

---
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/context/event/AbstractApplicationEventMulticaster.java
@@ -0,0 +1,81 @@
+package com.wychmod.springframework.context.event;
+
+import com.wychmod.springframework.beans.BeansException;
+import com.wychmod.springframework.beans.factory.BeanFactory;
+import com.wychmod.springframework.beans.factory.BeanFactoryAware;
+import com.wychmod.springframework.context.ApplicationEvent;
+import com.wychmod.springframework.context.ApplicationListener;
+import com.wychmod.springframework.utils.ClassUtils;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * @description: 提供了将事件广播给所有注册的监听器的能力。它维护了一个监听器列表，并在事件发生时，遍历列表将事件发送给所有注册的监听器。
+ * 提供了注册和移除监听器的方法，用于动态地管理监听器列表。
+ * @author: wychmod
+ * @date: 2023/7/19
+ */
+public abstract class AbstractApplicationEventMulticaster implements ApplicationEventMulticaster, BeanFactoryAware {
+
+    public final Set<ApplicationListener<ApplicationEvent>> applicationListeners = new LinkedHashSet<>();
+
+    private BeanFactory beanFactory;
+
+    /**
+     * 添加事件监听器
+     */
+    @Override
+    public void addApplicationListener(ApplicationListener<?> listener) {
+        applicationListeners.add((ApplicationListener<ApplicationEvent>) listener);
+    }
+
+    @Override
+    public void removeApplicationListener(ApplicationListener<?> listener) {
+        applicationListeners.remove(listener);
+    }
+
+    @Override
+    public final void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    /**
+     * 返回与给定事件类型匹配的应用程序侦听器的集合。
+     */
+    protected Collection<ApplicationListener> getApplicationListeners(ApplicationEvent event) {
+        LinkedList<ApplicationListener> allListeners = new LinkedList<ApplicationListener>();
+        for (ApplicationListener<ApplicationEvent> listener : applicationListeners) {
+            if (supportsEvent(listener, event)) allListeners.add(listener);
+        }
+        return allListeners;
+    }
+
+    /**
+     * 监听器是否对该事件感兴趣
+     */
+    protected boolean supportsEvent(ApplicationListener<ApplicationEvent> applicationListener, ApplicationEvent event) {
+        Class<? extends ApplicationListener> listenerClass = applicationListener.getClass();
+
+        // 按照 CglibSubclassingInstantiationStrategy、SimpleInstantiationStrategy 不同的实例化类型，需要判断后获取目标 class
+        Class<?> targetClass = ClassUtils.isCglibProxyClass(listenerClass) ? listenerClass.getSuperclass() : listenerClass;
+        Type genericInterface = targetClass.getGenericInterfaces()[0];
+
+        Type actualTypeArgument = ((ParameterizedType) genericInterface).getActualTypeArguments()[0];
+        String className = actualTypeArgument.getTypeName();
+        Class<?> eventClassName;
+        try {
+            eventClassName = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new BeansException("wrong event class name: " + className);
+        }
+        // 判定此 eventClassName 对象所表示的类或接口与指定的 event.getClass() 参数所表示的类或接口是否相同，或是否是其超类或超接口。
+        // isAssignableFrom是用来判断子类和父类的关系的，或者接口的实现类和接口的关系的，默认所有的类的终极父类都是Object。如果A.isAssignableFrom(B)结果是true，证明B可以转换成为A,也就是A可以由B转换而来。
+        return eventClassName.isAssignableFrom(event.getClass());
+    }
+
+}
