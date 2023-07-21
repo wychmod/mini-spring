0a0c3d256c3818574d340c1405d4ad83e4947b01
wychmod <545480453@qq.com>
2023-07-21 15:33:31 +0800

    默认实现的资源处理器,对location进行判断，判断用哪个加载器

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/core/io/DefaultResourceLoader.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/core/io/DefaultResourceLoader.java
@@ -4,20 +4,21 @@ import cn.hutool.core.lang.Assert;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.MalformedInputException;
 
 /**
- * @description: 默认实现的资源处理器
+ * @description: 默认实现的资源处理器,对location进行判断，判断用哪个加载器
  * @author: wychmod
  * @date: 2023/7/14
  */
-public class DefaultResourceLoader implements ResourceLoader{
+public class DefaultResourceLoader implements ResourceLoader {
+
     @Override
     public Resource getResource(String location) {
-        Assert.notNull(location,"Location must not be null");
-        if (location.startsWith(ResourceLoader.CLASSPATH_URL_PREFIX)) {
-            return new ClassPathResource(location.substring(ResourceLoader.CLASSPATH_URL_PREFIX.length()));
-        } else {
+        Assert.notNull(location, "Location must not be null");
+        if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+            return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()));
+        }
+        else {
             try {
                 URL url = new URL(location);
                 return new UrlResource(url);
@@ -26,4 +27,5 @@ public class DefaultResourceLoader implements ResourceLoader{
             }
         }
     }
+
 }
