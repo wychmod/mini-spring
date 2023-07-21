46ea459a76fa345e55c4027896f8df3d7adf93d4
wychmod <545480453@qq.com>
2023-07-21 16:33:33 +0800

    应用上下文接口

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/context/ApplicationContext.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/context/ApplicationContext.java
@@ -1,12 +1,13 @@
 package com.wychmod.springframework.context;
 
+import com.wychmod.springframework.beans.factory.HierarchicalBeanFactory;
 import com.wychmod.springframework.beans.factory.ListableBeanFactory;
+import com.wychmod.springframework.core.io.ResourceLoader;
 
 /**
- * @description: 应用上下文接口 为应用程序提供配置的中央接口。它维护一个 BeanFactory，提供配置文件的基本功能。
+ * @description: 应用上下文接口,高级接口 为应用程序提供配置的中央接口。它维护一个 BeanFactory，提供配置文件的功能，发布事件，。
  * @author: wychmod
  * @date: 2023/7/17
  */
-public interface ApplicationContext extends ListableBeanFactory {
-
+public interface ApplicationContext extends ListableBeanFactory, HierarchicalBeanFactory, ResourceLoader, ApplicationEventPublisher  {
 }
