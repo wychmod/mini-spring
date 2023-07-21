dfe0ae7d64e267e817fb7b4bb33ac1448a4a0c81
wychmod <545480453@qq.com>
2023-07-21 15:14:08 +0800

    处理配置的bean工厂的接口，例如注册beanPostProcessor，销毁单例对象等等

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/config/ConfigurableBeanFactory.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -1,6 +1,9 @@
 package com.wychmod.springframework.beans.factory.config;
 
+import com.sun.istack.internal.Nullable;
 import com.wychmod.springframework.beans.factory.HierarchicalBeanFactory;
+import com.wychmod.springframework.core.convert.ConversionService;
+import com.wychmod.springframework.utils.StringValueResolver;
 
 /**
  * @description: 处理配置的bean工厂的接口，例如注册beanPostProcessor，销毁单例对象等等
@@ -29,4 +32,15 @@ public interface ConfigurableBeanFactory extends HierarchicalBeanFactory, Single
      * 解析给定字符串，如注释属性
      */
     String resolveEmbeddedValue(String value);
+
+    /**
+     * 指定用于转换属性值的Spring 3.0转换服务，作为JavaBeans PropertyEditors的替代方法。
+     */
+    void setConversionService(ConversionService conversionService);
+
+    /**
+     * 返回关联的转换服务（如果有）。
+     */
+    @Nullable
+    ConversionService getConversionService();
 }
