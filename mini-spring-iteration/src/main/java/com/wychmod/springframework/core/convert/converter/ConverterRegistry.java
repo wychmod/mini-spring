1793e1190be5dbdd7c3700b238e16273c8dea2bc
wychmod <545480453@qq.com>
2023-07-22 21:40:31 +0800

    类型转换注册接口

---
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/core/convert/converter/ConverterRegistry.java
@@ -0,0 +1,29 @@
+package com.wychmod.springframework.core.convert.converter;
+
+/**
+ * @description: 类型转换注册接口
+ * @author: wychmod
+ * @date: 2023/7/19
+ */
+public interface ConverterRegistry {
+
+    /**
+     * Add a plain converter to this registry.
+     * The convertible source/target type pair is derived from the Converter's parameterized types.
+     * @throws IllegalArgumentException if the parameterized types could not be resolved
+     */
+    void addConverter(Converter<?, ?> converter);
+
+    /**
+     * Add a generic converter to this registry.
+     */
+    void addConverter(GenericConverter converter);
+
+    /**
+     * Add a ranged converter factory to this registry.
+     * The convertible source/target type pair is derived from the ConverterFactory's parameterized types.
+     * @throws IllegalArgumentException if the parameterized types could not be resolved
+     */
+    void addConverterFactory(ConverterFactory<?, ?> converterFactory);
+
+}
