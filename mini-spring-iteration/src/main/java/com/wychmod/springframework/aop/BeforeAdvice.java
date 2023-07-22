4f649396a480a5cc8e776955b51a26cc22783009
wychmod <545480453@qq.com>
2023-07-22 15:04:45 +0800

    BeforeAdvice 接口用于定义前置通知（Before advice）。前置通知是一种在目标方法执行之前执行的通知，它可以在目标方法执行前执行一些额外的逻辑。

---
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/aop/BeforeAdvice.java
@@ -0,0 +1,12 @@
+package com.wychmod.springframework.aop;
+
+import org.aopalliance.aop.Advice;
+
+/**
+ * @description: BeforeAdvice 接口用于定义前置通知（Before advice）。前置通知是一种在目标方法执行之前执行的通知，它可以在目标方法执行前执行一些额外的逻辑。
+ * @author: wychmod
+ * @date: 2023/7/19
+ */
+public interface BeforeAdvice extends Advice {
+
+}
