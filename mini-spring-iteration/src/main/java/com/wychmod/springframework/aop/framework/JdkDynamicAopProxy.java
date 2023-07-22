6622588306837d68d8a2a91af20bf3118d259973
wychmod <545480453@qq.com>
2023-07-22 14:13:35 +0800

    JDK 动态代理

---
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/aop/framework/JdkDynamicAopProxy.java
@@ -0,0 +1,37 @@
+package com.wychmod.springframework.aop.framework;
+
+import com.wychmod.springframework.aop.AdvisedSupport;
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @description: JDK 动态代理
+ * @author: wychmod
+ * @date: 2023/7/19
+ */
+public class JdkDynamicAopProxy implements AopProxy, InvocationHandler {
+
+    private final AdvisedSupport advised;
+
+    public JdkDynamicAopProxy(AdvisedSupport advised) {
+        this.advised = advised;
+    }
+
+    @Override
+    public Object getProxy() {
+        return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), advised.getTargetSource().getTargetClass(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (advised.getMethodMatcher().matches(method, advised.getTargetSource().getTarget().getClass())) {
+            MethodInterceptor methodInterceptor = advised.getMethodInterceptor();
+            return methodInterceptor.invoke(new ReflectiveMethodInvocation(advised.getTargetSource().getTarget(), method, args));
+        }
+        return method.invoke(advised.getTargetSource().getTarget(), args);
+    }
+
+}
