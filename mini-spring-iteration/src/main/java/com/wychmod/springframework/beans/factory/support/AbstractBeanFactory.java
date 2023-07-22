8d4c89a33938150979e6713f98ca82270a34b83c
wychmod <545480453@qq.com>
2023-07-22 20:50:15 +0800

    抽象的 Bean 工厂基类，定义模板方法

--- mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/AbstractBeanFactory.java
+++ mini-spring-iteration/src/main/java/com/wychmod/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -2,16 +2,37 @@ package com.wychmod.springframework.beans.factory.support;
 
 import com.wychmod.springframework.beans.BeansException;
 import com.wychmod.springframework.beans.factory.BeanFactory;
+import com.wychmod.springframework.beans.factory.FactoryBean;
 import com.wychmod.springframework.beans.factory.config.BeanDefinition;
+import com.wychmod.springframework.beans.factory.config.BeanPostProcessor;
+import com.wychmod.springframework.beans.factory.config.ConfigurableBeanFactory;
+import com.wychmod.springframework.core.convert.ConversionService;
+import com.wychmod.springframework.utils.ClassUtils;
+import com.wychmod.springframework.utils.StringValueResolver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description: 抽象的 Bean 工厂基类，定义模板方法
  * 继承了 DefaultSingletonBeanRegistry，也就具备了使用单例注册类方法的能力。
- *
  * @author: wychmod
  * @date: 2023/7/14
  */
-public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport implements ConfigurableBeanFactory {
+
+    /** 类加载器，用于解析 Bean 类名 */
+    private ClassLoader beanClassLoader = ClassUtils.getDefaultClassLoader();
+
+    /** BeanPostProcessor 應用於 createBean */
+    private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<BeanPostProcessor>();
+
+    /**
+     * 字符串解析器应用于注释属性值
+     */
+    private final List<StringValueResolver> embeddedValueResolvers = new ArrayList<>();
+
+    private ConversionService conversionService;
 
     @Override
     public Object getBean(String name) throws BeansException {
@@ -23,19 +44,90 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
         return doGetBean(name, args);
     }
 
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+        return (T) getBean(name);
+    }
+
+    @Override
+    public boolean containsBean(String name) {
+        return containsBeanDefinition(name);
+    }
+
+    protected abstract boolean containsBeanDefinition(String beanName);
 
     protected <T> T doGetBean(final String name, final Object[] args) {
-        Object bean = getSingleton(name);
-        if (bean != null) {
-            return (T) bean;
+        Object sharedInstance = getSingleton(name);
+        if (null != sharedInstance) {
+            // 如果是 FactoryBean，则需要调用 FactoryBean#getObject
+            return (T) getObjectForBeanInstance(sharedInstance, name);
         }
 
         BeanDefinition beanDefinition = getBeanDefinition(name);
-        return (T) createBean(name, beanDefinition, args);
+        Object bean = createBean(name, beanDefinition, args);
+        return (T) getObjectForBeanInstance(bean, name);
+    }
+
+    /**
+     * 判断是否为 FactoryBean，如果是，则调用 FactoryBean#getObject
+     */
+    private Object getObjectForBeanInstance(Object beanInstance, String beanName) {
+        if (!(beanInstance instanceof FactoryBean)) {
+            return beanInstance;
+        }
+
+        Object object = getCachedObjectForFactoryBean(beanName);
+
+        if (object == null) {
+            FactoryBean<?> factoryBean = (FactoryBean<?>) beanInstance;
+            object = getObjectFromFactoryBean(factoryBean, beanName);
+        }
+
+        return object;
     }
 
     protected abstract BeanDefinition getBeanDefinition(String beanName);
 
     protected abstract Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args);
 
+    @Override
+    public void addBeanPostProcessor(BeanPostProcessor beanPostProcessor){
+        this.beanPostProcessors.remove(beanPostProcessor);
+        this.beanPostProcessors.add(beanPostProcessor);
+    }
+
+    @Override
+    public void addEmbeddedValueResolver(StringValueResolver valueResolver) {
+        this.embeddedValueResolvers.add(valueResolver);
+    }
+
+    @Override
+    public String resolveEmbeddedValue(String value) {
+        String result = value;
+        for (StringValueResolver resolver : this.embeddedValueResolvers) {
+            result = resolver.resolveStringValue(result);
+        }
+        return result;
+    }
+
+    @Override
+    public void setConversionService(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+    @Override
+    public ConversionService getConversionService() {
+        return conversionService;
+    }
+
+    /**
+     * 返回将应用于使用此工厂创建的 BeanPost 处理器的列表。
+     */
+    public List<BeanPostProcessor> getBeanPostProcessors() {
+        return this.beanPostProcessors;
+    }
+
+    public ClassLoader getBeanClassLoader() {
+        return this.beanClassLoader;
+    }
 }
